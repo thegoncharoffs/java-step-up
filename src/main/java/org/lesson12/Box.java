35e4e4adc3da2f7d36c38bf15060d14901530a67
Гончаров Борис Алексеевич (4069533) <goncharovb@dev.vtb.ru>
2024-01-25 21:38:57 +0300

    Fix

--- src/main/java/org/lesson12/Box.java
+++ src/main/java/org/lesson12/Box.java
@@ -4,29 +4,29 @@ public class Box {
     @BeforeSuite
     public static void beforeSuite() {
         System.out.println("beforeSuite");
-    };
+    }
 
     @AfterSuite
     public static void afterSuite() {
         System.out.println("afterSuite");
-    };
+    }
 
     @Test(priority = 3)
     public static void test3() {
         System.out.println("test3");
-    };
+    }
 
     @Test
     public static void testDefault() {
         System.out.println("testDefault");
-    };
+    }
 
     @Test(priority = 7)
     public static void test7() {
         System.out.println("test7");
-    };
+    }
 
     public static void noAnnotation() {
         System.out.println("noAnnotation");
-    };
+    }
 }
